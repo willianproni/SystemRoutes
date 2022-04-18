e9aac3f5e3f5c0905f3cc07652f277c855b5da12
Willian Proni <89309834+willianproni@users.noreply.github.com>
2022-04-18 09:15:51 -0300

    Delete MVCRouteSystem directory

--- MVCRouteSystem/Models/City.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MVCRouteSystem.Models
-{
-    public class City
-    {
-        public int Id { get; set; }
-        public string CityName { get; set; }
-        public string State { get; set; }
-    }
-}
